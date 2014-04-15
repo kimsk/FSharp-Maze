021bf9d37d0a7a5aff7b2f3cf31c6280994b9f79
Karlkim Suwamongkol <karlkim@outlook.com>
2014-04-14 20:59:17 -0500

    Clean up some code..
    Before merging to FSharp-Snippets

--- src/script.fsx
+++ src/script.fsx
@@ -6,14 +6,13 @@ type Block = Start | Finish | Path | Wall
 
 let mazeSize = 12
 let blockSize = 20
-let getMaze = Array2D.init mazeSize mazeSize
-let maze = getMaze (fun _ _ -> Path)
+let getMaze = Array2D.init
 
-let show title (maze:Block[,]) =
-    let height = ((Array2D.length1 maze) + 2) * blockSize
-    let width = ((Array2D.length2 maze) + 1) * blockSize
-    
-    let form = new Form(Text = title, Width=width, Height=height)
+let show (maze:Block[,]) =
+    let height = (Array2D.length1 maze) * blockSize
+    let width = (Array2D.length2 maze) * blockSize    
+//    let form = new Form(Text = title, Width = (width + 16), Height = (height + 39), ControlBox = false, ShowIcon = false)        
+    let form = new Form(FormBorderStyle = FormBorderStyle.None, Width = width, Height = height)
     let box = new PictureBox(BackColor = Color.Magenta, Dock = DockStyle.Fill)
     let image = new Bitmap(width, height)
     let graphics = Graphics.FromImage(image)
@@ -29,7 +28,7 @@ let show title (maze:Block[,]) =
         for c in 0..colsNum do
             match maze.[r,c] with
             | Wall -> 
-                let rect = new Rectangle(0 + (r*blockSize), 0 + (c*blockSize), blockSize, blockSize)
+                let rect = new Rectangle(c*blockSize, r*blockSize, blockSize, blockSize)
                 graphics.FillRectangle(brush, rect)
                 graphics.DrawRectangle(pen, rect)
             | _ -> ()
@@ -46,30 +45,34 @@ let rec getRandom min max (nums:int seq) =
 let getRandomMaze:seq<int>->int = getRandom 0 (mazeSize-1)
 
 let divideMaze wallR wallC height width =    
-    let topLeft = Array2D.init (wallR - 1) (wallC - 1) (fun r c -> Path)
-    let topRight = Array2D.init (wallR - 1) (width - wallC - 1) (fun r c -> Path)
+    let topLeft = Array2D.init (wallR) (wallC) (fun r c -> Path)
+    let topRight = Array2D.init (wallR) (width - wallC) (fun r c -> Path)
     let bottomLeft = Array2D.init (height - wallR - 1) (wallC) (fun r c -> Path)
-    let bottomRight = Array2D.init (height - wallR - 1) (width - wallC - 1) (fun r c -> Path)
-    topLeft |> show "top-left"
-    topRight |> show "top-right"
-    bottomLeft |> show "bottom-left"
-    bottomRight |> show "bottom-right"
+    let bottomRight = Array2D.init (height - wallR - 1) (width - wallC) (fun r c -> Path)    
+    topLeft |> printfn "%A"
+    topRight |> printfn "%A"
+    bottomLeft |> printfn "%A"
+    bottomRight |> printfn "%A"
 
 let buildWall (maze:Block[,]) =
-    let width = Array2D.length2 maze
     let height = Array2D.length1 maze
+    let width = Array2D.length2 maze
 
     let wallR = rnd.Next(0, height)
     let wallC = rnd.Next(0, width)
     let holeR = getRandomMaze [wallR]
     let holeC = getRandomMaze [wallC]
-
+    printfn "%d %d %d %d" wallR wallC height width
     divideMaze wallR wallC height width
-
-    getMaze (fun r c -> 
-                match r,c with
-                | r,c when r = wallR && c <> holeC -> Wall
-                | r,c when c = wallC && r <> holeR -> Wall
+    
+    getMaze height width (fun r c -> 
+                match r, c with
+                | _ when r = wallR && c <> holeC -> Wall
+                | _ when c = wallC && r <> holeR -> Wall
                 | _ -> maze.[r,c])
 
-maze |> buildWall |> show "maze"
+getMaze mazeSize mazeSize (fun _ _ -> Path) |> buildWall |> show
+
+getMaze 10 20 (fun _ _ -> Path) |> (fun m -> printfn "%d %d" (Array2D.length1 m) (Array2D.length2 m))
+
+getMaze 12 12 (fun r c -> (r,c)) |> printfn "%A"
